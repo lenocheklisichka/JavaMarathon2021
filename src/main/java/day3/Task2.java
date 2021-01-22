e8f98fa5eab6d3f744e7160e95ebfad5d40640ee
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2021-01-22 21:48:01 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            double z = x / y;
+            if (y == 0) {
+                break;
+            } else {
+                System.out.println(z);
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        for (int index = 0; index < 5; index++) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            double c = a / b;
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            if (b != 0) {
+                System.out.println(c);
+            }
+        }
     }
 }
