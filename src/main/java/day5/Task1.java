f46b62f3e452d24b1cf3fefbd410f226c1c043a4
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2021-01-25 13:57:23 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car skoda = new Car();
+        skoda.setYearOfIssue(2020);
+        skoda.setColor("White ");
+        skoda.setModel("Octavia ");
+        System.out.println("Skoda"
+                + "\nГод выпуска: " + skoda.getYearOfIssue()
+                + "\nЦвет: " + skoda.getColor()
+                + "\nМодель: " + skoda.getModel());
+    }
+}
 
+class Car {
+    private int yearOfIssue;
+    private String color;
+    private String model;
+
+    public void setYearOfIssue(int yearOfIssue) {
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,34 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike kawasaki = new Motorbike(2019, "black", "Performance Sport");
+        System.out.println("KAWASAKI"
+                + "\nГод выпуска: " + kawasaki.getYearOfIssue()
+                + "\nЦвет: " + kawasaki.getColor()
+                + "\nМодель: " + kawasaki.getModel());
+    }
+}
 
+class Motorbike {
+    private int yearOfIssue;
+    private String color;
+    private String model;
+
+    public Motorbike(int yearOfIssue, String color, String model) {
+        this.yearOfIssue = yearOfIssue;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
