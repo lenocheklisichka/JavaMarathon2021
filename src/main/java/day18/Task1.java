db4d9ee608d9c8a6f8dac4ea02c0e42831652333
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2021-02-09 18:43:23 +0300

    complete day18 tasks

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,21 @@
 package day18;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(recursionSum(numbers));
+    }
 
+    public static int recursionSum(int[] numbers) {
+        int sum = 0;
+        int length = numbers.length;
+        int[] copyNumbers;
+        if (length > 0) {
+            copyNumbers = Arrays.copyOf(numbers, length -1);
+            sum += recursionSum(copyNumbers) + numbers[length - 1];
+        }
+        return sum;
     }
 }
